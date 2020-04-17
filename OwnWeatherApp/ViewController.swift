d929b072ea9e114db24038986a6209c80bb8571d
detymek <tymon.gf@hotmail.com>
2020-04-17 10:44:24 +0200

    Initial Commit

---
+++ OwnWeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OwnWeatherApp
+//
+//  Created by Daniel Golęba Frygies on 17/04:108.
+//  Copyright © 2020 Tymon Golęba Frygies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
